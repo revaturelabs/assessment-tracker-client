884c3319bad5ff9a0dcfe3431dfa47cfba1997db
Maria-Santi <misantibanez7@gmail.com>
2021-07-19 15:04:21 -0500

    assessment dashboard

---
+++ newJS/admin_assessments.js
@@ -0,0 +1,5 @@
+// getAllCategories() to get the tech stacks (/categories route)
+
+// Need a route to get all assessments by category
+// to display in the table, and get their average
+// getAssessmentByCategory()
