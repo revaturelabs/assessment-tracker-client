21bcbf6ee5673934f775600e0a76c0feb44d32cc
jkgmr13 <jokell13552@gmail.com>
2021-07-20 12:57:36 -0600

    actually added admin assessments docs

--- newJS/admin_assessments.js
+++ newJS/admin_assessments.js
@@ -2,7 +2,10 @@ const pythonPath = "http://ec2-34-204-173-118.compute-1.amazonaws.com:5000";
 const bucketPath = "http://adam-ranieri-batch-1019.s3.amazonaws.com";
 let java_base_url = "http://34.204.173.118:7001/";
 
-// populates the tech stacks tables with every assessment and average
+/**
+ * populates the tech stacks tables with every assessment and average
+ * @param {String} category The name of the category 
+ */
 async function getAssessments(category){
     const response = await fetch(`http://34.204.173.118:7001/categories/${category}/averages`);
     const assessments = await response.json();
@@ -20,7 +23,11 @@ async function getAssessments(category){
 
 }
 
-// getAllCategories() to get the tech stacks (/categories route)
+
+/**
+ * Creates tables for each all the tech stack categories
+ */
+
 // currently the database is not set properly for categories related to assessments
 async function getAllCategories(){
     const response = await fetch(`http://34.204.173.118:7001/categories`);
@@ -49,7 +56,10 @@ async function getAllCategories(){
         getAssessments(cat.name);
     }
 
-    // populates the tech stacks tables with every assessment and average
+    /**
+     * populates the tech stacks tables with every assessment and average
+     * @param {String} category The name of the tech stack
+     */
     async function getAssessments(category){
         const response = await fetch(`http://34.204.173.118:7001/categories/${category}/averages`);
         const assessments = await response.json();
@@ -67,36 +77,19 @@ async function getAllCategories(){
     
     }
 }
-
-// function to call many functions when the window loads
-// for testing
-function addLoadEvent(func){
-    var oldonload = window.onload;
-    if (typeof window.onload != 'function'){
-        window.onload = func;
-    }
-    else { 
-    window.onload = function() { 
-        if (oldonload) { 
-            oldonload(); 
-        } 
-        func(); 
-        } 
-    }
-}
-
+/**
+ * Clears the users session storage
+ */
 function wipeStorage() {
     sessionStorage.clear();
 }
 
-// logout function
+/**
+ * Logs the user out and redirects them to the homepage
+ */
 function logout(){
 	document.location = bucketPath + "/home.html"
 }
-
-// addLoadEvent(getAssessments("Python"));
-// addLoadEvent(getAssessments("Java"));
-// addLoadEvent(getAssessments("Test"));
 
 getAllCategories();
 
